8f18e002975a7b187594edd1e073b3715967506a
Daniel Ohuruogu <daniel.ohuruogu@hotmail.co.uk>
2022-05-05 18:33:32 +0100

    final git commit - final version with minimal styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import metronome from './metronome_sound.mp3'
+import useAudio from './useAudio';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [isTapTempo, setIsTapTempo] = useState(false);
+
+    const [timeStampArr, setTimeStampArr] = useState([])
+
+    const [tempoForMet, setTempoForMet] = useState(0);
+
+    const [bpm, setBpm] = useState()
+
+    // const [displayBPM, setDisplayBPM] = useState([])
+
+    function trackTime(e) {
+        if (isTapTempo) {
+            console.log("trying to see time in event data")
+            setTimeStampArr(timeStampArr => timeStampArr.concat(e.timeStamp));
+        }
+    }
+
+    function calcTempo() {
+        // will need to find average time between each click of the button
+        var timeGapSum = 0;
+
+        if (timeStampArr.length >= 4) {
+            // calculate the tempo from the last 4 strikes of the button, and only the last 4 strikes
+            for (let n = timeStampArr.length - 1, i = n; i >= n - 2; i--) {
+                console.log("i is ", i)
+                console.log(timeStampArr[i])
+                var timeGap = timeStampArr[i] - timeStampArr[i-1]
+                console.log("this time gap is ",timeGap)
+                timeGapSum += timeGap;
+            }
+            // calc average using sum
+            console.log("timeGapSum is ", timeGapSum)
+            var timeGapAve = timeGapSum / 3;
+            console.log("average time gap is ",timeGapAve)
+            return timeGapAve
+        }
+        return null
+    }
+
+    useEffect(()=> {
+        if (isTapTempo) {
+            if (calcTempo() != null) {
+                setTempoForMet(calcTempo())
+            }
+            // setIsTapTempo(true)
+            console.log(tempoForMet);
+        }
+
+        setBpm(Math.round(60000/tempoForMet, 2))
+    }, [isTapTempo, timeStampArr])
+
+    const [playing, toggle] = useAudio(metronome, isTapTempo, tempoForMet, 
+        // setDisplayBPM
+        );
+
+    const toggleTrack = () => setIsTapTempo(!isTapTempo)
+
+
+    return (
+        <div className="App">
+            <h1>Metronome app</h1>
+            <h3>Default is 60 BPM</h3>
+            
+            <div className='button-container'>
+                    <button onClick={toggle}>{playing ? "Pause" : "Play"}</button>
+
+                    <button onClick={toggleTrack}>{isTapTempo ? "Stop" : "Set tempo"}</button>
+                    {isTapTempo && 
+                    (<>
+                        <button onClick={trackTime}>Click</button>
+                        <p>(click at least 4 times - last 4 clicks are recorded)</p>
+                        <p>Hit stop when done</p>
+                    </>)
+                    }
+            </div>
+            <div className='bpm-header'>
+                <p>Set BPM: {bpm === Infinity ? "0" : bpm} BPM</p>
+            </div>
+            {/* <div className='bpm-display'>
+                <p>{displayBPM.map((item)=> item)}</p>
+            </div> */}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/useAudio.js
@@ -0,0 +1,67 @@
+import {useEffect, useState} from "react"
+import useSound from 'use-sound'
+
+
+const useAudio = (url, isTapTempo, tempo=null, 
+    // setDisplayBPM
+    ) => {
+
+    // const audio = new Audio(url);
+
+    const [play, {stop}] = useSound(url);
+
+    const [playing, setPlaying] = useState(false);
+
+    const toggle = () => setPlaying(!playing);
+
+    const [interId, setInterId] = useState();
+
+    // will have to find a way to get rid of existing interval when the button is clicked and play the new tempo
+    // I think some kind of state management
+
+    // it's only when isTapTempo is false and there is a tempo calculated that a new interval is set up
+
+    useEffect(() => {
+        console.log("tempo is ",tempo);
+        clearInterval(interId)
+        // setDisplayBPM([])
+        // if play button has been clicked to toggle it on - first point of call
+        if (playing) {
+            if (!tempo) {
+                console.log("is playing?: ",playing)
+                setInterId(setInterval(()=> {
+                    console.log("tick")
+                    play()
+                    // setDisplayBPM(displayBPM => displayBPM.concat("."))
+                }, 1000))
+            } else {
+                // tempo is something in here
+                if (!isTapTempo) {
+                    setInterId(setInterval(()=>{
+                        console.log("tick with tempo")
+                        play()
+                        // setDisplayBPM(displayBPM => displayBPM.concat("."))
+                    }, tempo))
+                }
+                // if taptempo is true, user is choosing their tempo - change nothing
+            }
+        } else {
+            console.log("is playing?: ",playing)
+            stop();
+            clearInterval(interId);
+            console.log("after clearing, interval id was ",interId)
+        }
+    }, [playing, isTapTempo]);
+
+    // useEffect(() => {
+    //     audio.addEventListener("ended", () => setPlaying(false));
+    //     return () => {
+    //         audio.removeEventListener("ended", ()=> setPlaying(false));
+    //     }
+    // }, [])
+
+
+    return [playing, toggle];
+}
+
+export default useAudio
